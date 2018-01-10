437a7b04212310316d1938d50c184bc03361c941
jagrati <jagratikatariya1996@gmail.com>
2018-01-10 16:20:35 +0530

    Barcode scanner added and dialog box on bar code scanner made..again with small changes

--- app/src/main/java/org/poornima/aarohan/aarohan_2018forcoorfinators/EventAllDetailsActivity.java
+++ app/src/main/java/org/poornima/aarohan/aarohan_2018forcoorfinators/EventAllDetailsActivity.java
@@ -1,13 +1,23 @@
 package org.poornima.aarohan.aarohan_2018forcoorfinators;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.barcode.Barcode;
 
 import org.poornima.aarohan.aarohan_2018forcoorfinators.Fragments.AbsentFrag;
 import org.poornima.aarohan.aarohan_2018forcoorfinators.Fragments.AllAbsentFrag;
@@ -25,6 +35,7 @@ public class EventAllDetailsActivity extends AppCompatActivity {
 
     private TabLayout tablayout;
     private ViewPager viewpager;
+    private static final int RC_BARCODE_CAPTURE = 9001;
 
 
 
@@ -40,6 +51,15 @@ public class EventAllDetailsActivity extends AppCompatActivity {
 
         tablayout = findViewById(R.id.tabs);
         tablayout.setupWithViewPager(viewpager);
+
+        FloatingActionButton scan = findViewById(R.id.scanner);
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(EventAllDetailsActivity.this,BarcodeCaptureActivity.class);
+                startActivityForResult(intent,RC_BARCODE_CAPTURE);
+            }
+        });
 
 
     }
@@ -80,6 +100,37 @@ public class EventAllDetailsActivity extends AppCompatActivity {
         @Override
         public CharSequence getPageTitle(int position) {
             return getmFragmentTitleList.get(position);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+
+        if (requestCode==RC_BARCODE_CAPTURE)
+        {
+
+            LayoutInflater factory = LayoutInflater.from(EventAllDetailsActivity.this);
+            final View dialogview= factory.inflate(R.layout.dialog_scanner_result,null);
+            final AlertDialog dialog = new AlertDialog.Builder(EventAllDetailsActivity.this).create();
+            dialog.setView(dialogview);
+            TextView regno = dialogview.findViewById(R.id.nametxt);
+            if(resultCode== RC_BARCODE_CAPTURE)
+            {
+                if(data!=null)
+                {
+                    Log.d("Data", data.getStringExtra("barcodeValue"));
+                    String barcodeValue = data.getStringExtra("barcodeValue");
+                    regno.setText(barcodeValue);
+                    dialog.show();
+
+                }
+            }
+
+        }
+
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
         }
     }
 }
